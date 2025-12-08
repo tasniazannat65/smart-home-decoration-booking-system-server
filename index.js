4612d4f6ebcd07fc89c5143bb1fafe2422e22acb
tasniazannat65 <tasniazannat9@gmail.com>
2025-12-08 13:08:18 +0600

    Added MongoDB Atlas integration with environment-based credentials and verified successful server connection.

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-const express = require('express')
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const port = process.env.PORT || 3000;
 // middleware
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('Welcome to Laxius Decor')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gwyrdqg.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+app.get("/", (req, res) => {
+  res.send("Welcome to Laxius Decor");
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Laxius decor in running on port: ${port}`)
-})
+  console.log(`Laxius decor in running on port: ${port}`);
+});
