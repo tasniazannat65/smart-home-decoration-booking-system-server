1dcc65d8bf99c3ec15396ae0fdee7c32da3ea76b
tasniazannat65 <tasniazannat9@gmail.com>
2025-12-10 22:20:36 +0600

    Create /services API with search and filter support

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
+const  admin = require("firebase-admin");
+
+const  serviceAccount = require("./laxius_decor_firebase_adminsdk_key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -27,6 +34,7 @@ async function run() {
     await client.connect();
         const db = client.db('laxius_decor');
         const userCollections = db.collection('users');
+        const serviceCollections = db.collection('services');
 
         // user related API's
         app.post('/users', async(req, res)=>{
@@ -43,6 +51,47 @@ async function run() {
 
 
         })
+
+        app.get('/users/:email/role', async(req, res)=>{
+      const email = req.params.email;
+      const query = {email};
+      const user = await userCollections.findOne(query);
+      res.send({role: user?.role || 'user'})
+    })
+  
+    // services related API's
+    app.get('/services', async(req, res)=>{
+      try {
+           const search = req.query.search || "";
+      const category = req.query.category || "";
+      let min =   parseInt(req.query.min);
+      let max =   parseInt(req.query.max);
+      if(isNaN(min)){
+        return min = 0;
+      }
+      if(isNaN(max)){
+        return max = 9999999;
+      }
+      let query = {
+        cost: {$gte: min, $lte: max}
+      };
+      if(search){
+        query.service_name = {$regex: search, $options: 'i'};
+      }
+      if(category){
+        query.service_category = category;
+      }
+      const result = await serviceCollections.find(query).toArray();
+      res.send(result);
+        
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({message: 'Server error'});
+        
+      }
+   
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
